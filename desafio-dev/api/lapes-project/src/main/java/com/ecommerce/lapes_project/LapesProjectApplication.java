8792e9c797386c6de4569c1c08099759c1ad9713
LeonardoGomes-eng <leonardoagomes.eng@gmail.com>
2026-07-14 00:15:23 -0300

    chore: setup inicial do projeto spring boot

---
+++ desafio-dev/api/lapes-project/src/main/java/com/ecommerce/lapes_project/LapesProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.lapes_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LapesProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LapesProjectApplication.class, args);
+	}
+
+}
---
+++ desafio-dev/api/lapes-project/src/test/java/com/ecommerce/lapes_project/LapesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.lapes_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LapesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
